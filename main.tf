902df144586672259b7d18c3e6bc3c658f2933e3
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2025-01-30 10:19:54 +0900

    feat: Cleaned up unused things. Fixed web app registry auth

--- main.tf
+++ main.tf
@@ -36,13 +36,19 @@ resource "azurerm_linux_web_app" "example" {
     container_registry_use_managed_identity = false
 
     application_stack {
-      docker_image_name   = "nginx:latest"
-      docker_registry_url = "https://index.docker.io"
+      docker_image_name   = "bootcamp:latest"
+      docker_registry_url = "https://${azurerm_container_registry.registry.login_server}"
+      docker_registry_username = azurerm_container_registry.registry.admin_username
+      docker_registry_password = azurerm_container_registry.registry.admin_password
     }
   }
 
   app_settings = {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+  }
+
+  lifecycle {
+    ignore_changes = [ site_config[0].application_stack[0].docker_image_name ]
   }
 }
 
@@ -52,19 +58,4 @@ resource "azurerm_container_registry" "registry" {
   location            = azurerm_resource_group.example.location
   sku                 = "Basic"
   admin_enabled       = true
-}
-
-resource "azurerm_user_assigned_identity" "example" {
-  location            = azurerm_resource_group.example.location
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_federated_identity_credential" "example" {
-  name                = "bootcamp"
-  resource_group_name = azurerm_resource_group.example.name
-  audience            = ["api://AzureADTokenExchange"]
-  issuer              = "https://token.actions.githubusercontent.com"
-  parent_id           = azurerm_user_assigned_identity.example.id
-  subject             = "repo:CodeSeoul/seollal-bootcamp-2025-backend:environment:production"
 }
