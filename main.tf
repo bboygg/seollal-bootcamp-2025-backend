34fa6f49143a13db16c833646b1592064715f4cb
sungjo-go <sungjo.go@linecorp.com>
2025-01-30 13:44:07 +0900

    update for Github Action Session

--- main.tf
+++ main.tf
@@ -11,12 +11,9 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_subscription" "current" {
-}
-
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "Korea South"
+  location = "West Europe"
 }
 
 resource "azurerm_service_plan" "example" {
@@ -27,16 +24,17 @@ resource "azurerm_service_plan" "example" {
   sku_name            = "B1"
 }
 resource "azurerm_linux_web_app" "example" {
-  name                = "example-web-app-codeseoul-beege" # Must be globally unique
+  name                = "example-web-app-codeseoul-george94" # Must be globally unique
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   service_plan_id     = azurerm_service_plan.example.id
+
 
   site_config {
     container_registry_use_managed_identity = false
 
     application_stack {
-      docker_image_name   = "bootcamp:latest"
+      docker_image_name   = "bootcamp2025george:latest"
       docker_registry_url = "https://${azurerm_container_registry.registry.login_server}"
       docker_registry_username = azurerm_container_registry.registry.admin_username
       docker_registry_password = azurerm_container_registry.registry.admin_password
@@ -48,14 +46,14 @@ resource "azurerm_linux_web_app" "example" {
   }
 
   lifecycle {
-    ignore_changes = [ site_config[0].application_stack[0].docker_image_name ]
+    ignore_changes = [ site_config[0].application_stack[0].docker_image_name ] # [0] means let it use the first one 
   }
 }
 
 resource "azurerm_container_registry" "registry" {
-  name                = "bootcamp"
+  name = "bootcamp2025george"
   resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "Basic"
-  admin_enabled       = true
+  location = azurerm_resource_group.example.location
+  sku = "Basic"
+  admin_enabled = true
 }
